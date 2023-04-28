4e762d0df3727575ab93ad795a6c4dee260c7786
Harsha <santoshsriharsha@gmail.com>
2023-04-28 09:47:34 +0530

    Add Modal and associated components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { Fragment } from "react";
 import "./App.css";
 import Header from "./components/Layouts/Header/Header";
 import Meals from "./components/Layouts/Meals/Meals/Meals";
+import Cart from "./components/Layouts/Cart/Cart/Cart";
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <Meals />
     </Fragment>
--- src/components/Layouts/Cart/Cart/Cart.js
+++ src/components/Layouts/Cart/Cart/Cart.js
@@ -1,4 +1,5 @@
 import classes from "./Cart.module.css";
+import Modal from "../../UI/Modal/Modal";
 
 const Cart = () => {
   const cartItems = (
@@ -10,7 +11,7 @@ const Cart = () => {
   );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -20,7 +21,7 @@ const Cart = () => {
         <button className={classes["button--alt"]}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
