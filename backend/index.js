f0d77fafc2dbe1f785c169ae61bf5f9a9d4abc91
Harsha <santoshsriharsha@gmail.com>
2023-05-25 22:41:06 +0530

    Create REST APIs and modify React code to use the APIs

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,18 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
+const AvailableMeals = require("./Models/AvailableMeals");
+
+mongoose
+  .connect("mongodb://localhost:27017/reactmeals", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => console.log(err));
+
 const app = express();
 app.listen(8080, () => {
   console.log("Listening on port 8080");
@@ -10,6 +23,14 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/", (req, res) => {
-  res.send("Welcome to React Meals backend");
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.get("/meals", async (req, res) => {
+  const availableMeals = await AvailableMeals.find({}).sort({ id: 1 });
+  res.json(availableMeals);
 });
