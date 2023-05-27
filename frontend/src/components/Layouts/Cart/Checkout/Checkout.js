61c81b04afdf21fa93b9d9edbcd231f0b789293d
Harsha <santoshsriharsha@gmail.com>
2023-05-27 16:56:43 +0530

    Modify styling of the Checkout form

--- frontend/src/components/Layouts/Cart/Checkout/Checkout.js
+++ frontend/src/components/Layouts/Cart/Checkout/Checkout.js
@@ -3,21 +3,25 @@ import classes from "./Checkout.module.css";
 const Checkout = (props) => {
   return (
     <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor="name">Name: </label>
-        <input type="text" id="name" />
+      <div className={classes.row}>
+        <div className={classes.control}>
+          <label htmlFor="name">Name: </label>
+          <input type="text" id="name" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="area">Area: </label>
+          <input type="text" id="area" />
+        </div>
       </div>
-      <div className={classes.control}>
-        <label htmlFor="area">Area: </label>
-        <input type="text" id="area" />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor="city">City: </label>
-        <input type="text" id="city" />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor="pincode">Pincode: </label>
-        <input type="text" id="pincode" />
+      <div className={classes.row}>
+        <div className={classes.control}>
+          <label htmlFor="city">City: </label>
+          <input type="text" id="city" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="pincode">Pincode: </label>
+          <input type="text" id="pincode" />
+        </div>
       </div>
       <div className={classes.actions}>
         <button type="button" onClick={props.onClose}>
